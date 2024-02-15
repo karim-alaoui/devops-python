7e78fb145eaae034a0080e7a428e675142285fc8
karim-alaoui <karim3alaoui@gmail.com>
2024-02-15 23:14:11 +0900

    created endpoint for getting list of continents

--- app.py
+++ app.py
@@ -16,5 +16,10 @@ def countries():
     countries_list = ["Morocco", "USA", "Canada", "UK", "Australia", "Germany"]
     return jsonify(countries_list)
 
+@app.route('/continents')
+def countries():
+    continents_list = ["Asia", "Africa", "North America", "South America", "Antarctica", "Europe", "Australia"]
+    return jsonify(continents_list)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
